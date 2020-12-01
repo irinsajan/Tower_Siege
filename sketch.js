895d79a9661a9d472809461c8b79c023b609243f
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-12-01 07:54:38 +0530

    Update sketch.js
    removing all the unnecessary lines and adding required lines

--- sketch.js
+++ sketch.js
@@ -1,43 +1,28 @@
-var a;
-var circles=[];
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
 var floor;
 
 function setup() {
   createCanvas(800,800);
 
+  engine = Engine.create();
+  world = engine.world;
+
   floor = new Ground(400, 700, 900, 5);
 
-  a=height;
-  circles.push(width/2)
+  
 }
 
 function draw() {
   background(153, 250, 97);
-  
-  
-  a=a-1;
-  
-  for (i=0;i<circles.length;i++)
-{
-	circle(circles[i], height/2,20)
-}
-if(camera.position.x%width===0)
-{
-	circles.push(camera.position.x+width/2)
-}
 
-floor.display();
+  Engine.update(engine);
+  
+ 
+  floor.display();
 
  drawSprites();
 }
-
-function keyPressed ()
-{
-  if(keyCode === RIGHT_ARROW)
-  {
-    if(keyIsDown(RIGHT_ARROW))
-    {
-      camera.position.x=camera.position.x+10;
-    }
-  }
-} 
